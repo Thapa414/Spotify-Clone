6f1f9d0f509ff5f5a10ef1173814af52b7a00eed
Thapa414 <147171795+Thapa414@users.noreply.github.com>
2025-01-24 00:13:47 +0530

    2nd commit

--- login.js
+++ login.js
@@ -1,17 +1,17 @@
 let signUpBtn = document.getElementById("signUpBtn");
 let signInBtn = document.getElementById("signInBtn");
-let UserName = document.getElementById("UserName");
+let email = document.getElementById("email");
 let title = document.getElementById("title");
 
 signInBtn.addEventListener('click',()=>{
-    UserName.style.maxHeight = "0";
+    email.style.maxHeight = "0";
     title.innerText = "Sign In";
     signUpBtn.classList.add("disable");
     signInBtn.classList.remove("disable");
 })
 
 signUpBtn.addEventListener('click',()=>{
-    UserName.style.maxHeight = "60px";
+    email.style.maxHeight = "60px";
     title.innerText = "Sign Up";
     signUpBtn.classList.remove("disable");
     signInBtn.classList.add("disable");
